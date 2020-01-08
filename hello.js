ab7ef68cf43dd137617155dd04353350447d0918
hayzarava <gustitrian04@gmail.com>
2020-01-08 17:01:09 +0700

    membuat modul

--- hello.js
+++ hello.js
@@ -1,13 +1,15 @@
 //console.log("Hello World");
 
 var http = require("http");
+var hitungan = require("./belajar");
 
 const server = http.createServer();
 
-server.on("request", (req, res) => {
+server.on("request", (req, res) => { 
+    let hasil = hitungan.kali(30, 25);
     console.log("Request masuk");
-    res.writeHead(200, {"Content-type": "json"});
-    res.write('{ nama : "gusty" }');
+    res.writeHead(200, {"Content-type": "html"});
+    res.write(hasil.toString());
     res.end();
 });
 
